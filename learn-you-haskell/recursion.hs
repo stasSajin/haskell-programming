29aab6b8df936040cd287d7fef96d2eedd9b65ee
stassajin <sajin.stanislav@gmail.com>
2018-06-17 09:14:31 -0700

    finished recursion chapter

--- learn-you-haskell/recursion.hs
+++ learn-you-haskell/recursion.hs
@@ -20,3 +20,37 @@ take' n _
     | n<=0 = []
 take' _ [] = []
 take' n (x:xs) = x: take' (n-1) xs
+
+
+-- Recursive Reverse
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+
+-- Recursive Repeat
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+
+-- Recursive Zip
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ []  = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+
+-- Recursive elem
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+  | a == x = True
+  | otherwise = elem' a xs
+
+-- Recusrive quicksort
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerSorted = quicksort [a | a <- xs, a<=x]
+      biggerSorted = quicksort [a | a <- xs, a>x]
+  in smallerSorted ++ [x] ++ biggerSorted
